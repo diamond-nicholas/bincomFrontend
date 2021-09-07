34452abfa717a102a28fff158f637e1f5481d42d
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-09-07 11:51:19 +0100

    fetch all polling unit from the db

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -5,40 +6,61 @@ import axios from 'axios';
 import './home.css';
 
 const home = () => {
-  // const [allPol, setAllPol] = useState([]);
-  // const [error, setError] = useState('');
-  // const [loading, setLoading] = useState(false);
+  const [allPol, setAllPol] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  // const getAllPol = async () => {
-  //   try {
-  //     const res = await axios.get('https://bincomproj.herokuapp.com/api/v1/pu');
-  //     setLoading(true);
-  //     setAllPol(res.data);
-  //   } catch (error) {
-  //     setError(error.message, 'something went wrong while requesting posts');
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+  const getAllPol = async () => {
+    try {
+      const res = await axios.get('https://bincomproj.herokuapp.com/api/v1/pu');
+      setLoading(true);
+      setAllPol(res.data);
+    } catch (error) {
+      setError(error.message, 'something went wrong while requesting posts');
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  // useEffect(() => {
-  //   getAllPol();
-  // }, []);
+  useEffect(() => {
+    getAllPol();
+  }, []);
   // console.log({
-  //   event: allPol,
+  //   event: allPol.data?.pollingUnits.rows,
   // });
 
-  // if (error) return <h1>{error}</h1>;
+  if (error) return <h1>{error}</h1>;
+
   return (
     <div>
       <div className='container'>
-        <h1>Welcome to this Bincom interview project</h1>
+        <h1>
+          <span>B</span>
+          <span>i</span>
+          <span>n</span>
+          <span>c</span>
+          <span>o</span>
+          <span className='last'>m</span> interview project
+        </h1>
 
-        <p>
-          This is a simple project built with Node js and express, which uses
+        <p className='about'>
+          This is a project built with Node js and express, which uses
           Postgresql as a database and React as the frontend
         </p>
-        <button className='btn'>Getting started</button>
+        <div className='getallpol'>
+          <div className='inner'>
+            <h2>This fetches the polling units data</h2>
+            <div className='show'>
+              {allPol.data?.pollingUnits.rows.map(
+                ({ uniqueid, polling_unit_name, polling_unit_number }) => (
+                  <p key={uniqueid} className='card-heading'>
+                    {`${polling_unit_name}`}: {`${polling_unit_number}`}
+                  </p>
+                )
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
